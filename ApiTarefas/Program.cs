dbdadaa2af943dc1d648272915da0ce73ecf774b
Nalbert, Vinicius <rullaxfox@gmail.com>
2022-05-05 22:39:40 -0300

    minimalapi

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+   opt.UseInMemoryDatabase("TarefasDB")
+);
 
 var app = builder.Build();
 
@@ -18,3 +24,28 @@ app.MapGet("/", () => "Olá mundo");
 
 app.Run();
 
+app.MapGet("/tarefas", async (AppDbContext db) =>
+{
+    return await db.Tarefas.ToListAsync();
+});
+
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
+{
+    db.Tarefas.Add(tarefa);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tarefas/{ tarefa.Id }", tarefa);
+});
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
